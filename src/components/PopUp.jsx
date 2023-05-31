d83613ebec79c447995e3b24286d261f8d630aaa
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2023-05-31 14:49:15 -0600

    fixed error in link button

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -4,6 +4,9 @@ const PopUp = ({popup, setPopup})=>{
     const handleClose = () =>{
         setPopup(false)
     }
+    const handleClick = () => {
+        return window.location.href = 'https://www.brettbuhler.com'
+    }
     return (
         <>
 
@@ -12,7 +15,7 @@ const PopUp = ({popup, setPopup})=>{
                     <h2 className="popup-h2">Thank you for the visit!</h2>
                     <p className="popup-p">This site is out of date, visit my current portfolio at the link below</p>
                     <div className="popup-button-div">
-                        <button onClick={(()=>console.log('hi'))} className="popup-button">brettbuhler.com</button>
+                        <button onClick={handleClick} className="popup-button">brettbuhler.com</button>
                         <button onClick={handleClose} className="popup-button">Close</button>
                     </div>
                 </div>
